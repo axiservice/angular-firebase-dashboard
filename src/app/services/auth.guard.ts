2b098b487746dcae2c6ebd3cbfa75e996c38dab5
Ryan Logsdon <ryan@ryanlogsdon.com>
2021-03-17 18:10:41 -0700

    added checks for email verification status

--- src/app/services/auth.guard.ts
+++ src/app/services/auth.guard.ts
@@ -21,10 +21,8 @@ export class AuthGuard implements CanActivate {
             this.afAuth.onAuthStateChanged((user) => {
                 if (user) {
 
-                    // if (user.emailVerified)
-                    //
-                    //
-                    //
+                    // if (!user.emailVerified)                            // if the user hasn't verified their email, send them to that page
+                    //     this.router.navigate(['/verify-email']);
 
                     resolve(true);
                 } else {
